43407536ddfa839573221a6bb13501d1a663d3e6
MJJoy49 <mdjoy1477@gmail.com>
2026-01-20 21:37:13 +0600

    update

--- admin/pages/bills.php
+++
@@ -1,5 +0,0 @@
-<div class="bills">
-    ai file bills a kaj korte hobe------
-    asset/css/pagesPartCss/bills.css ar kaj
-    asset/css/pagesPartJs/bills.js ar kaj
-</div>
--- messManagement/assets/js/pagesPartJs/dashboard.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("totalMembers").innerText = 32;
-    document.getElementById("totalRooms").innerText = 10;
-});
